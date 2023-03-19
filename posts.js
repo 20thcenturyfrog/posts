e083cdfd278349469415c0935f92946bd22ee24b
20thcenturyfrog <pogorelova.katerina@yandex.ru>
2023-03-19 23:07:24 +0300

    Added styles

--- posts.js
+++ posts.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
 function createPost(p) {
   const post = document.createElement("div");
   post.className = "post";
-  post.innerHTML = `<h1 class="title">Заголовок: ${p.title}</h1><p class="article"><b>Статья:</b> ${p.body}</p>`;
+  post.innerHTML = `<h1 class="title">Заголовок: ${p.title}</h1><p class="body"><b>Статья:</b> ${p.body}</p>`;
   document.body.appendChild(post);
 }
 
